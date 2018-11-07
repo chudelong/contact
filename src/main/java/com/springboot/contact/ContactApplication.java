3442738944cdb2268a429f075363dee64c5a6e12
chudelong <1114553919@qq.com>
2018-11-07 20:07:07 +0800

    -

--- src/main/java/com/springboot/contact/ContactApplication.java
+++ src/main/java/com/springboot/contact/ContactApplication.java
@@ -1,12 +1,16 @@
 package com.springboot.contact;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class ContactApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ContactApplication.class, args);
 	}
 }
